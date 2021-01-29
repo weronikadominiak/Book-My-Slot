624c27c463bb2809a8468bf6375c867ba32f935e
weronikadominiak <weronika.dominiak@hotmail.com>
2021-01-29 21:46:52 +0000

    Refactor the code, split functionalities into functions

--- src/program.py
+++ src/program.py
@@ -9,27 +9,30 @@ from selenium import webdriver
 emailStr = config("$email")
 pwdStr = config("$pwd")
 
-# Open the browser
-browser = webdriver.Chrome()
-browser.get(("https://www.tesco.com/groceries/en-GB/slots/delivery"))
 
-#Fill in user data
-userField = browser.find_element_by_id('username')
-userField.send_keys(emailStr)
-pwdFileld = browser.find_element_by_id('password')
-pwdFileld.send_keys(pwdStr)
+def open_browser():
+    browser = webdriver.Chrome()
+    browser.get("https://www.tesco.com/groceries/en-GB/slots/delivery")
 
-# Find button
-loginBtn = browser.find_element_by_class_name("ui-component__button")
-loginBtn.click()
+    # Fill in user data
+    user_field = browser.find_element_by_id('username')
+    user_field.send_keys(emailStr)
+    pwd_field = browser.find_element_by_id('password')
+    pwd_field.send_keys(pwdStr)
 
-# Find slots
-slots = browser.find_elements_by_class_name("slot-selector--3-week-tab-space")
+    # Find button
+    login_btn = browser.find_element_by_class_name("ui-component__button")
+    login_btn.click()
 
-### todo: Here put looking for an empty slots once you know how they look
-slots[1].click()
+    # Find slots
+    slots = browser.find_elements_by_class_name("slot-selector--3-week-tab-space")
 
-# Email sending
+    # todo: Here put looking for an empty slots once you know how they look
+    slots[1].click()
+
+
+# Send Email
+
 sender_email = "luckyducky.development@gmail.com"
 receiver_email = "weronika.dominiak3@gmail.com"
 message = """\
@@ -40,25 +43,32 @@ Hello, I booked a slot for you!
 
 This message is sent from Python."""
 
-port = 465  # for SSL
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+def send_email():
+    port = 465  # for SSL
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, pwdStr)
-    server.sendmail(sender_email, receiver_email, message)
+    # Create a secure SSL context
+    context = ssl.create_default_context()
 
-
-### Todo: Schedule a job execution
-# https://schedule.readthedocs.io/en/stable/
-def job():
-    print("Yooo I'm fine!")
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, pwdStr)
+        server.sendmail(sender_email, receiver_email, message)
 
 
-schedule.every(10).seconds.do(job)
-schedule.every().day.at("8:01").do(job)
+def schedule_job():
+    # Todo: Schedule a job execution
+    # https://schedule.readthedocs.io/en/stable/
+    def job():
+        print("Yooo I'm fine!")
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    schedule.every(10).seconds.do(job)
+    # schedule.every().day.at("8:01").do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+open_browser()
+send_email()
+schedule_job()
